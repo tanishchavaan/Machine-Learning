a8975254bbd3defc2d59e7952a3582d4da63e6e1
Tanish Chavan <164027219+tanishchavaan@users.noreply.github.com>
2024-12-19 22:20:58 +0530

    Add files via upload

---
+++ first.cpp
@@ -0,0 +1,19 @@
+#include
+using namespace std;
+
+string solution(int n, int m) {
+    if (n < m) {
+        return "lesser";
+    } else if (n == m) {
+        return "equal";
+    } else {
+        return "greater";
+    }
+}
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    cout << solution(n, m) << endl;
+    return 0;
+}
